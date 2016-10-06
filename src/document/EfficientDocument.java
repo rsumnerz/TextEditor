f32a3a2e8fa92ac466a8e910aa6ea5a68ba6dc6d
Saadat Rizvi <saadatrezvi@gmail.com>
2016-10-07 03:01:31 +0500

    Week 3 done

--- src/document/EfficientDocument.java
+++ src/document/EfficientDocument.java
@@ -50,7 +50,28 @@ public class EfficientDocument extends Document {
 		// That is not a word or a sentence-ending puctuation.
 		// MAKE SURE YOU UNDERSTAND THIS LINE BEFORE YOU CODE THE REST
 		// OF THIS METHOD.
+		numSentences =0;
+		numSyllables =0;
+		numWords =0;
 		List<String> tokens = getTokens("[!?.]+|[a-zA-Z]+");
+		//System.out.println(tokens.size()); 
+		String token;
+		 
+		for ( int i=0;i<tokens.size();i++){
+			token = tokens.get(i);
+			if (isWord(token)){
+				numWords++;
+				numSyllables +=countSyllables(token);
+			
+				if (i==tokens.size()-1) numSentences++;
+			}
+			
+			else numSentences++;
+			
+			
+			
+		}
+		
 		
 		// TODO: Finish this method.  Remember the countSyllables method from 
 		// Document.  That will come in handy here.  isWord defined above will also help.
@@ -73,7 +94,7 @@ public class EfficientDocument extends Document {
 	@Override
 	public int getNumSentences() {
 		//TODO: write this method.  Hint: It's simple
-		return 0;
+		return numSentences;
 	}
 
 	
@@ -94,7 +115,7 @@ public class EfficientDocument extends Document {
 	@Override
 	public int getNumWords() {
 		//TODO: write this method.  Hint: It's simple
-	    return 0;
+	    return numWords;
 	}
 
 
@@ -116,7 +137,7 @@ public class EfficientDocument extends Document {
 	@Override
 	public int getNumSyllables() {
         //TODO: write this method.  Hint: It's simple
-        return 0;
+        return numSyllables;
 	}
 	
 	// Can be used for testing
